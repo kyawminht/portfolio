272338c8019326fabd65ea6f3e5ecf10a8148b34
KyawMinHtwe <104247470+kyawminht@users.noreply.github.com>
2024-02-22 20:04:03 +0630

    fixing responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,13 +9,13 @@ const Hero = () => {
     window.location.href = `mailto:${emailAddress}`;
   };
   return (
-    <section className=' scroll-smooth flex flex-col sm:flex-row justify-center items-center p-10 mt-5 space-x-10 ' id='about'>
+    <section className=' scroll-smooth flex flex-col sm:flex-row justify-center items-center p-10 mt-5 md:space-x-10 ' id='about'>
       <div className=' flex-1 font '>
         <p className="text-4xl mb-5 text-red-600">I am</p>
         <h2 className=' text-6xl font-bold mb-5 font dark:text-white font-serif'>Kyaw Min Htwe</h2>
         <p className="font font-semibold dark:text-white">A passionate and dedicated software developer with a love for creating innovative solutions. I specialize in web development, bringing ideas to life through code and design. Explore my portfolio and discover the possibilities!</p>
       </div>
-      <div className=' flex-1'>
+      <div className=' flex-1 mx-auto'>
         <img src={HeroImg} alt="" width={200} height={50} className=' rounded-full w-full border-8 border-white'/>
       </div>
       <div className=' flex-1'>
