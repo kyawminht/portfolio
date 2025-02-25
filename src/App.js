926415ad7d2c9811bbaf249b6adc7c3ef86ee0db
Kyaw Min Htwe <kyawminhtway288@gmail.com>
2025-02-25 20:21:28 +0630

    test gtag

--- src/App.js
+++ src/App.js
@@ -18,36 +18,36 @@ import { initGA, trackPageView, trackScrollDepth } from './analystics';
 
 function App() {
 
-   // Initialize GA4 when the app loads
-   useEffect(() => {
-    initGA();
-    trackPageView(window.location.pathname); // Track initial page view
-  }, []);
+  //  // Initialize GA4 when the app loads
+  //  useEffect(() => {
+  //   initGA();
+  //   trackPageView(window.location.pathname); // Track initial page view
+  // }, []);
 
-   // Scroll tracking logic
-   useEffect(() => {
-    const trackedPercentages = [25, 50, 75, 100]; // Track these percentages
-    const handleScroll = () => {
-      const scrollTop = window.scrollY;
-      const windowHeight = window.innerHeight;
-      const docHeight = document.documentElement.scrollHeight;
-      const totalScrollable = docHeight - windowHeight;
-      const scrolledPercentage = (scrollTop / totalScrollable) * 100;
+  //  // Scroll tracking logic
+  //  useEffect(() => {
+  //   const trackedPercentages = [25, 50, 75, 100]; // Track these percentages
+  //   const handleScroll = () => {
+  //     const scrollTop = window.scrollY;
+  //     const windowHeight = window.innerHeight;
+  //     const docHeight = document.documentElement.scrollHeight;
+  //     const totalScrollable = docHeight - windowHeight;
+  //     const scrolledPercentage = (scrollTop / totalScrollable) * 100;
 
-      trackedPercentages.forEach((percentage) => {
-        if (
-          scrolledPercentage >= percentage &&
-          !window[`scrolled_${percentage}`]
-        ) {
-          trackScrollDepth(percentage);
-          window[`scrolled_${percentage}`] = true; // Mark as tracked
-        }
-      });
-    };
+  //     trackedPercentages.forEach((percentage) => {
+  //       if (
+  //         scrolledPercentage >= percentage &&
+  //         !window[`scrolled_${percentage}`]
+  //       ) {
+  //         trackScrollDepth(percentage);
+  //         window[`scrolled_${percentage}`] = true; // Mark as tracked
+  //       }
+  //     });
+  //   };
 
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, []);
 
   return (
     <div className="App dark:bg-slate-800">
